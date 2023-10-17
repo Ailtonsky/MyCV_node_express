5e5e96aceff3a39e3895581fc273e96a70a9c041
ailtonsky <ailtonkanon@outlook.com>
2023-10-17 12:12:57 -0600

    agregacion de test1,2,3,4

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ app.get('/size', (req, res, next) => {
 app.get('/hours', (req, res, next) => {
 	res.sendfile("assets/1a_dev_hours.json");
 });
+
+app.get('/3a_test1', (req, res, next) => {
+	res.sendfile("assets/3a_test1.json");
+});
+
+app.get('/3a_test2', (req, res, next) => {
+	res.sendfile("assets/3a_test2.json");
+});
+
+app.get('/3a_test3', (req, res, next) => {
+	res.sendfile("assets/3a_test3.json");
+});
+
+app.get('/3a_test4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
